b45b5d8a7c667f9bed85e97cfcaf7013102c3809
Oleksandr Halushka <dsxxx95@gmail.com>
2023-02-12 12:46:22 +0200

    add lesson 10 task 1

---
+++ Homework/lesson10/WeekDayMain.java
@@ -0,0 +1,13 @@
+package lesson10;
+
+public class WeekDayMain {
+    public static void main(String[] args) {
+        Weekday [] weekdays = Weekday.values();
+
+        for (Weekday el: weekdays) {
+            System.out.println(el.toString() +  " is a workday - " + el.isWeekDay());
+        }
+
+    }
+
+}
