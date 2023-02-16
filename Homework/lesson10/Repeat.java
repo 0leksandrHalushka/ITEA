cec0a6ea1d63cae54ff0345bce3a1011cea77373
Oleksandr Halushka <dsxxx95@gmail.com>
2023-02-16 18:23:32 +0200

    add out

---
+++ Homework/lesson10/Repeat.java
@@ -0,0 +1,14 @@
+package lesson10;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Repeat {
+    int repeating() default 3;
+    String repeatingText () default "Runnng!";
+
+}
---
+++ Homework/lesson10/Runner.java
@@ -0,0 +1,25 @@
+package lesson10;
+
+//@Repeat
+
+@Repeat(repeating = 2,
+        repeatingText = "Hello!")
+
+public  class Runner implements Running {
+
+    @Override
+    public void run() {
+        Class<Runner> runnerClass = Runner.class;
+        Repeat repeat = runnerClass.getAnnotation(Repeat.class);
+
+        for (int i = 0; i < repeat.repeating(); i++) {
+            System.out.println(repeat.repeatingText());
+        }
+    }
+
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.run();
+    }
+}
+
