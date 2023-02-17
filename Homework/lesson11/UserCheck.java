a37509767c4219d300b79f7f7729871ff149614f
Oleksandr Halushka <dsxxx95@gmail.com>
2023-02-17 11:49:44 +0200

    corrections in task 11

--- Homework/lesson11/UserCheck.java
+++ Homework/lesson11/UserCheck.java
@@ -1,32 +1,30 @@
 package lesson11;
 
 public class UserCheck {
-    public static void checkUser (String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+
+    public static boolean checkUser (String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
 
         boolean onlyLatinAlphabetLogin = login.matches("^[a-zA-Z0-9_]+$");
         boolean onlyLatinAlphabetPassword = password.matches("^[a-zA-Z0-9_]+$");
 
-        if (onlyLatinAlphabetLogin && login.length() < 20 ) {
-            System.out.println("Login is ok!");
-        } else {
+        if (!onlyLatinAlphabetLogin || login.length() >= 20 ) {
             throw new WrongLoginException("Incorrect login");
-        }
-
-        if (onlyLatinAlphabetPassword && password.length() <20 && password.equals(confirmPassword)) {
-            System.out.println("Password is ok!");
-        } else {
+        } else if (!onlyLatinAlphabetPassword || password.length() > 20 || !password.equals(confirmPassword)) {
             throw new WrongPasswordException("Incorrect password");
+        } else {
+            return true;
         }
 
     }
 
     public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
+        boolean resultCheck = false;
 
         try {
-            checkUser("1234567890qwertyuio", "1234567890qwertyuio", "1234567890qwertyuio1");
-        } catch (WrongPasswordException exception) {
-            System.err.println(exception);
-        } catch (WrongLoginException exception){
+            resultCheck = checkUser("1234567890qwerty_1", "1234567890qwertyui111", "1234567890qwertyui1");
+            System.out.println(resultCheck);
+        } catch (WrongPasswordException | WrongLoginException exception) {
+            System.out.println(resultCheck);
             System.err.println(exception);
         }
 
